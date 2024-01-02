656ddb0fa5754a548694d7a51a2b00ef900e5192
Akshitha22 <chinthakindiakshitha@gmail.com>
2024-01-02 10:59:09 +0530

    added files

--- BasicHashing/src/Main.java
+++ BasicHashing/src/Main.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n;
-        n = sc.nextInt();
+//        this is a program to study hashing by counting the occurrence of each number in the array by creating a hash array
+        Scanner in = new Scanner(System.in);
+
+//        enter the size of an array
+        System.out.print("Enter the size of the array: ");
+        int n = in.nextInt();
+
+//        enter the array elements
+        System.out.print("Enter the array elements: ");
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = in.nextInt();
         }
 
-        //precompute:
+//        creating a hash array of size 13
         int[] hash = new int[13];
         for (int i = 0; i < n; i++) {
-            hash[arr[i]] += 1;
-        }
+            hash[arr[i]] = hash[arr[i]] + 1;      // here we are counting the occurrence of each num in the array
+        }  //hash arr is ready now
 
-        int q;
-        q = sc.nextInt();
+//        enter the query size
+        System.out.print("Enter the query size: ");
+        int q = in.nextInt();
+//        take input queries to search for the occurrence numbers in hash array
         while (q-- != 0) {
-            int number = sc.nextInt();
-            // fetching:
-            System.out.print(hash[number]);
+            int nums = in.nextInt();
+//            fetching the frequency of arr elements
+            System.out.println(hash[nums]);
         }
     }
 }
