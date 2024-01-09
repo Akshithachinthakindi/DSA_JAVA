7644ced801e1c7bd7132d0233b662e36420a576a
Akshitha22 <chinthakindiakshitha@gmail.com>
2024-01-09 16:36:40 +0530

    added files

---
+++ Placement Prep/Arrayss/src/BuyAndSellStock.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BuyAndSellStock {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = in.nextInt();
+        System.out.print("Enter arr elements: ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println("The maximum profit after buying and selling stocks is: " + myFunc(arr, n));
+
+    }
+    static int myFunc(int[] arr, int n) {
+        int buy_time = arr[0];
+        int profit = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < buy_time) {
+                buy_time = arr[i];
+            } else {
+                int curProfit = arr[i] - buy_time;
+                profit = Math.max(curProfit, profit);
+            }
+        }
+        return profit;
+    }
+}
