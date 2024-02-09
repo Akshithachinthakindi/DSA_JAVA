86cfcc2ce2f3d4d1b6238af3b1d5617f01a5e23d
Akshitha22 <chinthakindiakshitha@gmail.com>
2024-02-09 12:27:42 +0530

    added files

--- CSE406/src/GUI/Button_Action1.java
+++ CSE406/src/GUI/Button_Action1.java
@@ -2,17 +2,51 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class MyFrame extends JFrame{
+class MyFrame extends JFrame implements ActionListener {
     Container c;
     JButton btn;
+    JButton btn2;
+    JButton btn3;
     MyFrame() {
         c = this.getContentPane();
         c.setLayout(null);
-        btn = new JButton("Click me");
-        btn.setBounds(200,200,150,50);
+        btn = new JButton("Orange");
+        btn2 = new JButton("Green");
+        btn3 = new JButton("Blue");
+        btn.setBounds(100,200,150,50);
         btn.setFont(new Font("Arial", Font.ITALIC, 24));
+        btn.setForeground(Color.orange);
+        btn.setBackground(Color.DARK_GRAY);
         c.add(btn);
+        btn.addActionListener(this); // this --> refers to current object i.e., button
+
+        btn2.setBounds(300,200,150,50);
+        btn2.setFont(new Font("Arial", Font.ITALIC, 24));
+        btn2.setForeground(Color.GREEN);
+        btn2.setBackground(Color.DARK_GRAY);
+        c.add(btn2);
+        btn2.addActionListener(this);
+
+        btn3.setBounds(500,200,150,50);
+        btn3.setFont(new Font("Arial", Font.ITALIC, 24));
+        c.add(btn3);
+        btn3.setForeground(Color.BLUE);
+        btn3.setBackground(Color.DARK_GRAY);
+        btn3.addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==btn)
+            c.setBackground(Color.ORANGE);
+        if (e.getSource() == btn2)
+            c.setBackground(Color.GREEN);
+        if (e.getSource() == btn3)
+            c.setBackground(Color.BLUE);
     }
 }
 public class Button_Action1 {
